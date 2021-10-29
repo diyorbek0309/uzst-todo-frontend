a4f4515c270fa81e66155546fa4b824cd417a1d3
diyorbek0309 <diyorbekolimov2000@gmail.com>
2021-10-30 00:52:11 +0500

    changed baseUrl

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://uzst-todo-backend.herokuapp.com/",
   headers: {
     "Content-Type": "application/json",
   },
